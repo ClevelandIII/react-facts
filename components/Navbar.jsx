b41352e96c0c4ba05b9709494f177acb9cd0a805
Cleveland Williams <Kingslime108@gmail.com>
2026-04-27 21:29:21 -0700

    Init

---
+++ components/Navbar.jsx
@@ -0,0 +1,25 @@
+/**
+ * Challenge: complete the Navbar to match the Figma design
+ *
+ * Hints:
+ * - for semantic HTML purposes, the Navbar should render
+ *   a <header> with a <nav> nested inside. The image and "ReactFacts"
+ *   text elements can both be rendered as children inside the <nav>
+ * - reference the Figma design for the most accurate info about
+ *   colors, sizes, font information, etc.
+ */
+
+function Navbar() {
+    return (
+        <>
+            <header>
+                <nav>
+                    <img src="src/assets/react.svg" alt="React Logo" />
+                    <p>ReactFacts</p>
+                </nav>
+            </header>
+        </>
+    );
+}
+
+export default Navbar;
